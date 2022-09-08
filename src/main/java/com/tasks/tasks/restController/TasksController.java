2d66a7a94363b3b1ac721a8ccb9e6759b7d803d7
kraz255 <65384298+kraz255@users.noreply.github.com>
2022-09-08 16:46:38 +0600

    добавлено логгирование

--- src/main/java/com/tasks/tasks/restController/TasksController.java
+++ src/main/java/com/tasks/tasks/restController/TasksController.java
@@ -2,16 +2,21 @@ package com.tasks.tasks.restController;
 
 import com.tasks.tasks.core.TaskItem;
 import com.tasks.tasks.core.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/v2")
 public class TasksController {
+
+    Logger logger = LoggerFactory.getLogger(TasksController.class);
 
     private final TaskRepository taskRepository;
 
@@ -20,29 +25,29 @@ public class TasksController {
     }
 
     @GetMapping("/list")
-    public Iterable<TaskItem> getAllTasks () {
+    public Iterable<TaskItem> getAllTasks(HttpServletRequest request) {
         return this.taskRepository.findAll();
     }
 
     @GetMapping("/list/{id}")
-    public Optional<TaskItem> getTaskById ( @PathVariable("id") Integer id) {
-
-        if (this.taskRepository.findById(id).isPresent()) {
+    public Optional<TaskItem> getTaskById(HttpServletRequest request, @PathVariable("id") Integer id) {
+        if(this.taskRepository.findById(id).isPresent()) {
             return this.taskRepository.findById(id);
         }
 
+        logger.warn("Requested task <ID {}> does not exist. {} {}", id, request.getMethod(), request.getRequestURL());
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
     }
 
     @DeleteMapping("/del/{id}")
-    public void deleteTaskById (@PathVariable("id") Integer id) {
+    public void deleteTaskById(HttpServletRequest request, @PathVariable("id") Integer id) {
 
-        if (this.taskRepository.findById(id).isPresent()) {
+        if(this.taskRepository.findById(id).isPresent()) {
             this.taskRepository.deleteById(id);
             throw new ResponseStatusException(HttpStatus.OK);
-        }
-        else {
+        } else {
+            logger.warn("Requested task <ID {}> does not exist. {} {}", id, request.getMethod(), request.getRequestURL());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
@@ -50,34 +55,36 @@ public class TasksController {
     }
 
     @PostMapping("/new")
-    public void addNewTask(@RequestBody TaskItem testTaskItem) {
+    public void addNewTask(HttpServletRequest request, @RequestBody TaskItem testTaskItem) {
 
         testTaskItem.setCreateTime(Instant.now().getEpochSecond());
         TaskItem newTestTaskItem = this.taskRepository.save(testTaskItem);
 
-        if (this.taskRepository.findById(testTaskItem.getId()).isPresent()) {
+        if(this.taskRepository.findById(testTaskItem.getId()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.CREATED);
         }
 
+        logger.error("Error save task in DB. {} {}", request.getMethod(), request.getRequestURL());
         throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PutMapping("/mod/{id}")
-    public TaskItem updateTask( @PathVariable("id") Integer id, @RequestBody TaskItem t) {
+    public TaskItem updateTask(HttpServletRequest request, @PathVariable("id") Integer id, @RequestBody TaskItem t) {
 
 
         Optional<TaskItem> taskToUpdateOptional = this.taskRepository.findById(id);
-        if (!taskToUpdateOptional.isPresent()) {
+        if(!taskToUpdateOptional.isPresent()) {
+            logger.warn("Requested task <ID {}> does not exist. {} {}", id, request.getMethod(), request.getRequestURL());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found");
             //return null;
         }
         TaskItem taskToUpdate = taskToUpdateOptional.get();
 
-        if (t.getTaskTitle() != null) {
+        if(t.getTaskTitle() != null) {
             taskToUpdate.setTaskTitle(t.getTaskTitle());
         }
 
-        if (t.getTaskDescription() != null) {
+        if(t.getTaskDescription() != null) {
             taskToUpdate.setTaskDescription(t.getTaskDescription());
         }
 
